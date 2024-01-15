4cb13ccda92498ac540522c961eeac850ea595d6
David Asabina <vid@bina.me>
2024-01-15 16:07:22 +0100

    chore: Stub devenv: Python + venv + commented Poetry

--- devenv.nix
+++ devenv.nix
@@ -2,27 +2,28 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env.GREET = "🦜🔗";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
-  '';
+  packages = with pkgs; [
+    git
+    # FIXME: Poetry breaks on incompat xattr, see err at end
+    # python311Packages.poetry
+  ];
 
   # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
+  languages.python = {
+    enable = true;
+    version = "3.11";
+    # FIXME: Poetry breaks on incompat xattr, see err at end
+    # ```
+    # poetry = {
+    #   enable = true;
+    #   install.enable = true;
+    # };
+    venv.enable = true;
+  };
+  # ```
+  # > Checking runtime dependencies for poetry-1.7.1-py3-none-any.whl
+  #   >   - xattr<0.11.0,>=0.10.0 not satisfied by version 1.0.0
 }
